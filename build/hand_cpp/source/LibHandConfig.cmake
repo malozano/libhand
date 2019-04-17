aa53454392e069625f75a4d1ab26ab582f7abeb5
Miguel Angel <malozano@ua.es>
2019-04-17 09:57:27 +0200

    Removed build files

--- build/hand_cpp/source/LibHandConfig.cmake
+++
@@ -1,7 +0,0 @@
-CMAKE_MINIMUM_REQUIRED (VERSION 2.8.6)
-
-include("${CMAKE_CURRENT_LIST_DIR}/LibHand-export.cmake")
-
-SET(LibHand_FOUND TRUE)
-SET(LibHand_INCLUDE_DIRS /home/mvrlab/Documentos/libhand/hand_cpp/source;/usr/include;/usr/include;/usr/include/opencv;/usr/include/OGRE;/usr/include;/usr/include;/usr/include/OGRE/RenderSystems/GL;/usr/include/OGRE/Plugins/BSPSceneManager;/usr/include/OGRE/Plugins/OctreeSceneManager;/usr/include/OGRE/Plugins/PCZSceneManager;/usr/include/OGRE/Plugins/ParticleFX)
-SET(LibHand_LIBRARIES -ldl -lXt -lboost_filesystem -lboost_system hand_hog hand_renderer hand_utils)
--- build/hand_cpp/source/dot_sceneloader/CMakeFiles/dot_sceneloader.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libdot_sceneloader.a"
-)
